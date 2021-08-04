1bc7c76a5e82a73d433f282c0bffac18103b78f6
Philip <philip.b@bluefusesystems.com>
2021-08-04 16:17:27 +0100

    service changes

--- src/app/shared/services/circuits.service.ts
+++ src/app/shared/services/circuits.service.ts
@@ -8,16 +8,68 @@ export class CircuitsService {
 
   constructor(private api: ApiService) { }
 
+  /**
+   * Return all the circuits that were part of the F1 grand prix
+   * circuit in a given season.
+   *
+   * @param season
+   * @returns
+   */
   getCircuitsBySeason(season: string) {
     return this.api.get(`${season}/circuits`);
   }
 
+  /**
+   * Return all the circuits that were part of the F1 grand prix
+   * circuit in a given season that a particular driver drove on.
+   *
+   * @param season
+   * @param driverId
+   */
+  getCircuitsBySeasonAndDriver(season: string, driverId: string) {
+
+  }
+
+  /**
+   * Return all the circuits that a particular driver has driven on.
+   *
+   * @param driverId
+   */
+  getCircuitsByDriver(driverId: string) {
+
+  }
+
+  /**
+   *  Return all the circuits driven by a particular constructor.
+   *
+   * @param constructorId
+   */
+  getCircuitsByConstructor(constructorId: string) {
+
+  }
+
+  /**
+   * Get the circuit that was used as part of the grand prix
+   * for the provided season and round.
+   *
+   * @param season
+   * @param round
+   * @returns
+   */
   getCircuitBySeason(season: string, round: string) {
     return this.api.get(`${season}/${round}/circuits`);
   }
 
   getCircuitById(id: string) {
     return this.api.get(`circuits/${id}`);
+  }
+
+  getCircuitRacesBySeason(season: string) {
+
+  }
+
+  getCircuitDriverStandingsBySeason(season: string) {
+
   }
 
 }
---
+++ src/app/shared/services/pitstops.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PitstopsService {
+
+  constructor() { }
+
+  getPistopsByCircuit(circuitId: string) {
+
+  }
+
+  getPistopsBySeason(season: string) {
+
+  }
+
+  getPistopsBySeasonAndRound(season: string, round: string) {
+
+  }
+
+}
